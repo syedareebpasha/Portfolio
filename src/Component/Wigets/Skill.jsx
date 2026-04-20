745b1f877ffefc762239be6af82c5db0c099a603
syedareebpasha <syedareebpasha9999@gmail.com>
2026-04-21 00:26:42 +0500

    changes

--- src/Component/Wigets/Skill.jsx
+++ src/Component/Wigets/Skill.jsx
@@ -14,7 +14,7 @@ import { FaReact } from "react-icons/fa6";
 import { color } from 'framer-motion'
 
 const skills = [
-  {icon: FaReact,             pct: '50%' , name: 'React',       color:'#61dafb', classNmae: 'react'},
+  {icon: FaReact,             pct: '50%' , name: 'React',       color:'#61dafb', className: 'react'},
   { icon: IoLogoHtml5,       pct: '95%', name: 'HTML5',       color: '#e34f26', className:'html' },
   { icon: FaCss3Alt,         pct: '90%', name: 'CSS3',        color: '#2965f1' , className:'css'},
   { icon: IoLogoJavascript,  pct: '70%', name: 'JavaScript',  color: '#f7df1e' ,className:'javascript'},
